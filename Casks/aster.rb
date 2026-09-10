aa2c1cb5c5ff0575eec292cb497f84b00c21621e
youseonghyeon <dolla_@naver.com>
2026-09-10 13:33:28 +0900

    fix(cask): use the current macos dependency syntax
    Homebrew 스타일 검사에 맞춰 최소 macOS 의존성 표기를 정리한다.

--- Casks/aster.rb
+++ Casks/aster.rb
@@ -8,7 +8,7 @@ cask "aster" do
   homepage "https://github.com/youseonghyeon/aster"
 
   auto_updates true
-  depends_on macos: ">= :high_sierra"
+  depends_on macos: :high_sierra
 
   app "Aster.app"
 end
